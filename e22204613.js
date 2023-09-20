bb949a5262a2f612fc45fda5d86328afd23561de
bankai-josef <145619872+bankai-josef@users.noreply.github.com>
2023-09-21 01:14:53 +0200

    Add files via upload

---
+++ e22204613.js
@@ -0,0 +1,32 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const menuItems = document.querySelectorAll(".menu-item");
+    const menuImage = document.getElementById("menu-image");
+
+    menuItems.forEach((item) => {
+        item.addEventListener("click", function (event) {
+            event.preventDefault();
+            const imageName = item.getAttribute("data-image");
+            menuImage.src = imageName;
+            menuImage.style.display = "block";
+        });
+    });
+});
+
+// afficherNomUtilisateur.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Récupérez le nom d'utilisateur depuis le stockage local
+    var nomUtilisateur = localStorage.getItem("nom_utilisateur");
+
+    // Vérifiez si un nom d'utilisateur a été stocké
+    if (nomUtilisateur) {
+        // Affichez le nom d'utilisateur où vous le souhaitez sur la page
+        var nomUtilisateurElement = document.getElementById("nom-utilisateur");
+        if (nomUtilisateurElement) {
+            nomUtilisateurElement.textContent = "Bonjour, " + nomUtilisateur + " !";
+        }
+    }
+});
+
+
+
